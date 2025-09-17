0e64e281bd21556afbe8994ded01fc395cef6951
Mizzlan <chapain.ml+github@gmail.com>
2025-09-17 17:51:31 +0545

    Add files via upload

---
+++ stars.py
@@ -0,0 +1,9 @@
+c = "*"
+n = int(input("enter the give number: "))
+d=" "
+e=1
+for i in range(1,n+1):
+    if i ==1 or i==n:
+        print(f"{c*(n)}")
+    else:
+        print(f"{c}{d*(n-2)}{c}")
