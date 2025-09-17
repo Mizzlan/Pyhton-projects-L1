12d7d49c46f4682ec629d6008631c6f9f5fd473d
Mizzlan <chapain.ml+github@gmail.com>
2025-09-17 20:19:03 +0545

    Update stars.py

--- stars.py
+++ stars.py
@@ -1,9 +1,10 @@
 c = "*"
-n = int(input("enter the give number: "))
+n = int(input("enter the given number: "))
 d=" "
 e=1
 for i in range(1,n+1):
     if i ==1 or i==n:
         print(f"{c*(n)}")
     else:
+
         print(f"{c}{d*(n-2)}{c}")
